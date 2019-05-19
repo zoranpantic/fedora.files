031fedf339b63861a5665d393dadddd019dae11c
zoran <zoran.lj.pantic@gmail.com>
2019-05-19 10:54:15 +0200

    emacs seems to work ok now

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -242,8 +242,6 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(company-idle-delay 0)
- '(company-minimum-prefix-length 2)
  '(custom-enabled-themes (quote (odersky)))
  '(custom-safe-themes
    (quote
@@ -258,17 +256,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#1E2326" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
- '(company-scrollbar-bg ((t (:background "#ffffffffffff"))))
- '(company-scrollbar-fg ((t (:background "#ffffffffffff"))))
- '(company-tooltip ((t (:inherit default :background "#ffffffffffff"))))
- '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
- '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
-
+ '(default ((t (:inherit nil :stipple nil :background "#1E2326" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 126 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
 ;;; init.el ends here
- ;;'(company-scrollbar-bg ((t (:background "#ffffffffffff"))))
- ;;'(company-scrollbar-fg ((t (:background "#ffffffffffff"))))
- ;;'(company-tooltip ((t (:inherit default :background "#ffffffffffff"))))
- ;;'(company-tooltip-common ((t (:inherit font-lock-constant-face))))
- ;;'(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
-
